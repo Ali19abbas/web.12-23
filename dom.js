5090a58f27feeccd2e1ad8c9fa3dfb9f73582f54
Ali19abbas <aliabbas110.aa36@gmail.com>
2023-12-04 04:49:21 +0530

    Dom using className

---
+++ dom.js
@@ -0,0 +1,8 @@
+console.dir(document);
+console.log(document.domain);
+console.log(document.url);
+let items=document.getElementsByClassName('list-group-item');
+console.log(items)
+items[2].style.backgroundColor='green'
+for(var i=0;i<items.length;i++)
+items[i].style.fontWeight='bold'
