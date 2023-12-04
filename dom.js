5f3fbacff2a021ecb16befdaeb0e8b5713659013
Ali19abbas <aliabbas110.aa36@gmail.com>
2023-12-05 05:23:35 +0530

    creating node

--- dom.js
+++ dom.js
@@ -1,18 +1,15 @@
-/*console.dir(document);
-console.log(document.domain);
-console.log(document.url);
-let items=document.getElementsByClassName('list-group-item');
-console.log(items)
-items[2].style.backgroundColor='green'
-for(var i=0;i<items.length;i++)
-items[i].style.fontWeight='bold'
-let l=document.getElementsByTagName('li')
-l.style.backgroundColor='red' g*/
-let q=document.querySelectorAll('li')
-console.log(q)
-q[1].style.color='green'
-for(let i=0;i<q.length;i++)
-{
-    if(i%2==0)
-    q[i].style.backgroundColor='green'
-}
+let newDiv=document.createElement('div');
+newDiv.className='hello';
+newDiv.id='Hello';
+
+var newDivText=document.createTextNode('HEllo');
+
+newDiv.appendChild(newDivText);
+
+var container =document.querySelector(('header .container'));
+
+var h1=document.querySelector('header h1');
+
+console.log(newDiv)
+
+container.insertBefore(newDiv, h1);
