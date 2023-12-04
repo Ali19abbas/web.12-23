1d1c4ac3636ca826e91e57d965562cab9c1ed577
Ali19abbas <aliabbas110.aa36@gmail.com>
2023-12-05 04:35:50 +0530

    querry

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-console.dir(document);
+/*console.dir(document);
 console.log(document.domain);
 console.log(document.url);
 let items=document.getElementsByClassName('list-group-item');
@@ -7,4 +7,12 @@ items[2].style.backgroundColor='green'
 for(var i=0;i<items.length;i++)
 items[i].style.fontWeight='bold'
 let l=document.getElementsByTagName('li')
-l.style.backgroundColor='red'
+l.style.backgroundColor='red' g*/
+let q=document.querySelectorAll('li')
+console.log(q)
+q[1].style.color='green'
+for(let i=0;i<q.length;i++)
+{
+    if(i%2==0)
+    q[i].style.backgroundColor='green'
+}
