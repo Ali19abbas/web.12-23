686337b2f0cdd04597d05de1f250c3ad69ee2dd5
Ali19abbas <aliabbas110.aa36@gmail.com>
2023-12-06 04:30:18 +0530

    removeElement

--- dom.js
+++ dom.js
@@ -1,15 +1,67 @@
-let newDiv=document.createElement('div');
-newDiv.className='hello';
-newDiv.id='Hello';
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
-var newDivText=document.createTextNode('HEllo');
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
-newDiv.appendChild(newDivText);
+// Add item
+function addItem(e){
+e.preventDefault();
 
-var container =document.querySelector(('header .container'));
+// Get input value
+var newItem = document.getElementById('item').value;
 
-var h1=document.querySelector('header h1');
+// Create new li element
+var li = document.createElement('li');
+// Add class
+li.className = 'list-group-item';
+// Add text node with input value
+li.appendChild(document.createTextNode(newItem));
 
-console.log(newDiv)
+// Create del button element
+var deleteBtn = document.createElement('button');
 
-container.insertBefore(newDiv, h1);
+// Add classes to del button
+deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+// Append text node
+deleteBtn.appendChild(document.createTextNode('X'));
+
+// Append button to li
+li.appendChild(deleteBtn);
+
+// Append li to list
+itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e){
+if(e.target.classList.contains('delete')){
+if(confirm('Are You Sure?')){
+  var li = e.target.parentElement;
+  itemList.removeChild(li);
+}
+}
+}
+
+// Filter Items
+function filterItems(e){
+// convert text to lowercase
+var text = e.target.value.toLowerCase();
+// Get lis
+var items = itemList.getElementsByTagName('li');
+// Convert to an array
+Array.from(items).forEach(function(item){
+var itemName = item.firstChild.textContent;
+if(itemName.toLowerCase().indexOf(text) != -1){
+  item.style.display = 'block';
+} else {
+  item.style.display = 'none';
+}
+});
+}
